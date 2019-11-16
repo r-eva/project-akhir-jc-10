753588458af79ce075052940d8f5768ed5c56eec
regina <reginaevadewi@gmail.com>
2019-11-16 13:33:20 +0700

    updatespinner

--- ui-project-akhir/src/1.component/Register/WaitingEmailVerification.jsx
+++ ui-project-akhir/src/1.component/Register/WaitingEmailVerification.jsx
@@ -2,20 +2,26 @@ import React, { Component } from 'react';
 import queryString from 'query-string'
 import axios from 'axios'
 import {urlApi} from '../../helpers/database'
+import swal from 'sweetalert'
 
 class WaitingEmailVerification extends Component {
+    
+    state = {
+        btnResendEmailClick: false
+    }
 
     onBtnResendEmailClick = () => {
+        this.setState({btnResendEmailClick: true})
         var params = queryString.parse(this.props.location.search)
         axios.post(urlApi + 'user/resendemailconfirm', {
             email: params.email
         })
         .then((res) => {
-            alert(res.data.message)
+            this.setState({btnResendEmailClick: false})
+            swal (`${res.data.message}`, `Silakan cek email anda!`, 'success')
         })
         .catch((err) => {
-            console.log(err.response)
-            alert(err.response.data.message)
+            swal (`Email gagal dikirim.`, `Cek koneksi atau email anda!`, 'error')
         })
     }
 
@@ -24,7 +30,15 @@ class WaitingEmailVerification extends Component {
             <div className="p-5 m-5 text-center">
                 <h1 className="mt-5 pt-5">Silakan Periksa Email Anda untuk konfirmasi</h1>
                 <h4>Klik Button dibawah bila tidak menerima Emailnya</h4>
-                <input type="button" className="btn btn-primary" value="Resend Email" onClick={this.onBtnResendEmailClick}/>
+                {
+                    this.state.btnResendEmailClick
+                    ?
+                    <div className="spinner-border text-primary" role="status">
+                        <span className="sr-only">Loading...</span>
+                    </div>
+                    :
+                    <input type="button" className="btn btn-primary" value="Resend Email" onClick={this.onBtnResendEmailClick}/>
+                }   
             </div>
         );
     }
